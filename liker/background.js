c7c2b4f75b7f28bc80efc0de7bfccaf690a4520a
NatanGer97 <42692104+NatanGer97@users.noreply.github.com>
2023-05-12 14:17:20 +0300

    with random delay

--- liker/background.js
+++ liker/background.js
@@ -1,4 +1,4 @@
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+/* chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.type === "processLinks") {
       console.log("Message from popup.js:", message);
       const { links } = message;
@@ -55,6 +55,76 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
             callback();
           }
         });
+      }
+    }
+  });
+   */
+
+  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.type === "processLinks") {
+      console.log("Message from popup.js:", message);
+      const { links } = message;
+  
+      processLinks(links);
+  
+      function processLinks(links) {
+        if (links.length === 0) {
+          return;
+        }
+  
+        const firstLink = links.shift();
+  
+        const tabProperties = { url: firstLink, active: true };
+  
+        chrome.tabs.create(tabProperties, (tab) => {
+          waitForPageLoad(tab, links);
+        });
+      }
+  
+      function waitForPageLoad(tab, links) {
+        chrome.tabs.onUpdated.addListener(function listener(tabId, changeInfo) {
+          if (tabId === tab.id && changeInfo.status === "complete") {
+            chrome.tabs.onUpdated.removeListener(listener);
+  
+            setTimeout(() => {
+              performLogicInTab(tab, () => {
+                processNextLink(tab, links);
+              });
+            }, getRandomDelay()); // Random delay between links
+          }
+        });
+      }
+  
+      function processNextLink(tab, links) {
+        if (links.length === 0) {
+          return;
+        }
+  
+        const nextLink = links.shift();
+  
+        chrome.tabs.update(tab.id, { url: nextLink }, () => {
+          waitForPageLoad(tab, links);
+        });
+      }
+  
+      function performLogicInTab(tab, callback) {
+        setTimeout(() => {
+          chrome.tabs.sendMessage(tab.id, { type: "logic" }, (response) => {
+            console.log("Received response from content script:", response);
+            // Handle the response from the content script if needed
+  
+            // Call the callback function to proceed to the next link
+            if (typeof callback === "function") {
+              callback();
+            }
+          });
+        }, getRandomDelay()); // Random delay before performing logic
+      }
+  
+      function getRandomDelay() {
+        const minDelay = 1000; // Minimum delay in milliseconds
+        const maxDelay = 5000; // Maximum delay in milliseconds
+        return Math.floor(Math.random() * (maxDelay - minDelay + 1) + minDelay);
       }
     }
   });
